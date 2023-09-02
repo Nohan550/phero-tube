27a0a659d35ffa7d8469e217bc6c4a5c1eb19efa
Nohan550 <nohanmuttakim.bd@gmail.com>
2023-09-02 16:22:27 +0600

    content added

--- posts.js
+++ posts.js
@@ -25,8 +25,39 @@ const category = async () => {
 const categoryContent = async categoryId => {
   const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
   data = await response.json();
-  console.log(data)
+ 
+    const contentContainer = document.getElementById('card-container')
+ 
+    contentContainer.innerHTML="";
+
+  data.data.forEach(content =>{
+     const div = document.createElement('div')
+    div.innerHTML =`
+    <div class="card w-80 bg-base-100 shadow-xl px-3 py-3">
+    <figure><img class="w-72 h-48" src="${content?.thumbnail}" alt="S" /></figure>
+    <div class="flex gap-3 py-3 items-center  " >
+
+      <img class="w-10 h-10 rounded-full" src="${content?.authors[0]?.profile_picture}" alt="">
+      <h2 class="text-xl font-bold">
+        ${content?.title}
+      </h2>
+    </div>
+
+    <div class= "flex items-center gap-2">
+      <h2>${content?.authors[0]?.profile_name}</h2>
+      <img class="h-4 w-4" src="./icon/icons8-verified-17.png" alt="">
+    </div>
+     <div >
+     <h3>${content?.others?.views} views</h3>
+   
+     </div>
+  </div>
+    `
+   contentContainer.appendChild(div)
+  } )
+
+  // console.log(data.data)
 }
 
-
+categoryContent("1000")
 category();
